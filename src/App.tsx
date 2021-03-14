4d8b2aea78bf691c7b2678c6856efd33765f3583
Ian Sinnott <ian@iansinnott.com>
2021-03-15 00:21:34 +0800

    Improve error state

--- src/App.tsx
+++ src/App.tsx
@@ -86,7 +86,9 @@ function App() {
 
     if (!isValidEthAddr(s)) {
       console.warn("invalid addr", s);
-      return setError("That eth address appears to be invalid");
+      setEth("");
+      setError("That eth address appears to be invalid");
+      return;
     }
 
     setLoading(true);
@@ -222,7 +224,28 @@ function App() {
 
               {error && (
                 <div className="mb-4 sm:mb-8">
-                  <h3 style={{ color: "red" }}>{error}</h3>
+                  <div className="rounded-md bg-red-50 p-4">
+                    <div className="flex">
+                      <div className="flex-shrink-0">
+                        {/* <!-- Heroicon name: solid/x-circle --> */}
+                        <svg
+                          className="h-5 w-5 text-red-400"
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 20 20"
+                          fill="currentColor"
+                          aria-hidden="true">
+                          <path
+                            fill-rule="evenodd"
+                            d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z"
+                            clip-rule="evenodd"
+                          />
+                        </svg>
+                      </div>
+                      <div className="ml-3">
+                        <h3 className="text-sm font-medium text-red-800">{error}</h3>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               )}
               {eth && (
